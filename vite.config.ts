4a66bd39b07078de1913bfb5c3fb6177c5a99cce
jviou <156534180+jviou@users.noreply.github.com>
2025-08-29 14:06:10 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [react()],
   server: {
-    host: "::",
-    port: 8080,
+    host: true,
+    allowedHosts: ['crisis-app.capucins-angers.fr'],
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  preview: {
+    host: true,
+    port: 4173,
+    strictPort: true,
+    allowedHosts: ['crisis-app.capucins-angers.fr'], // << important
   },
-}));
+})
